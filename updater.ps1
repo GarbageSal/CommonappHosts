2d21f780f8cc397e56c363db0ee1192f47dc75f5
GarbageSal <30483813+GarbageSal@users.noreply.github.com>
2018-10-04 10:52:51 +0800

    Add files via upload

---
+++ updater.ps1
@@ -0,0 +1,21 @@
+﻿#Code By JerryZhao
+#2018/10/02
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -ArgumentList $arguments -Verb runAs
+	Exit
+}
+Write-Host "正在更新HOSTS..."
+Add-Content -Path "C:\Windows\System32\drivers\etc\hosts" -Value "
+# Commonapp Start
+54.243.116.198 www.commonapp.org
+13.35.121.58 apply.commonapp.org
+54.230.108.221 app.commonapp.org
+# Commonapp End
+" -Force
+Sleep(0.5)
+Write-Host "更新完成"
+Sleep(0.5)
+Pause
+Exit
